f14819df478c57860caba1c1755b7700ff3018ba
julianagomeshioki <127693280+julianagomeshioki@users.noreply.github.com>
2024-10-20 20:21:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var numberToFind = 0;
 var attempts = 0;
-var maxAttempts = 10; // Define o número máximo de tentativas
+var maxAttempts = 10; 
 
 function refresh() {
-    attempts = 0; // Reseta as tentativas
+    attempts = 0; 
     numberToFind = Math.floor(Math.random() * 101); 
     console.log(numberToFind); 
 }
@@ -12,13 +12,13 @@ function verifyNumber() {
     var bet = document.getElementById("bet").value; 
     bet = parseInt(bet);
 
-    // Verifica se a aposta é válida
+
     if (bet > 100 || bet < 0) {
         alert('Aposta Inválida'); 
         return;
     }
     
-    attempts++; // Incrementa o número de tentativas
+    attempts++; 
 
     if (bet > numberToFind) {
         alert('O Número a ser encontrado é menor');
@@ -30,13 +30,13 @@ function verifyNumber() {
         return;
     }
 
-    // Verifica se o número máximo de tentativas foi atingido
+    
     if (attempts >= maxAttempts) {
         alert(`Você esgotou suas tentativas! O número era ${numberToFind}.`);
-        refresh(); // Reinicia o jogo
+        refresh(); 
     } else {
         alert(`Você ainda tem ${maxAttempts - attempts} tentativas restantes.`);
     }
 }
 
-refresh(); // Inicia o jogo
+refresh(); 
